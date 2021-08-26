423cdced771fd49470d3ac66902840ca7ae95b77
eblondel <emmanuel.blondel1@gmail.com>
2021-08-26 07:03:49 +0200

    update package date

--- R/d4storagehub4R.R
+++ R/d4storagehub4R.R
@@ -19,7 +19,7 @@
 #'    Package: \tab d4storagehub4R\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.1\cr
-#'    Date: \tab 2021-04-14\cr
+#'    Date: \tab 2021-08-28\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
